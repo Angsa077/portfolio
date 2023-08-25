f804d0b6fd67bb740786bbf077f637b17878722e
Angga Saputra <angsa0077@gmail.com>
2023-08-25 15:21:13 +0700

    about

--- components/Socials.js
+++ components/Socials.js
@@ -13,19 +13,19 @@ import {
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-        <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <Link href={'https://github.com/Angsa077'} className='hover:text-accent transition-all duration-300'>
             <RiGithubLine />
         </Link>
-        <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <Link href={'https://www.instagram.com/angsa_77/'} className='hover:text-accent transition-all duration-300'>
             <RiInstagramLine />
         </Link>
-        <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <Link href={'https://www.tiktok.com/@angsa_77'} className='hover:text-accent transition-all duration-300'>
             <RiTiktokLine />
         </Link>
-        <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <Link href={'https://www.facebook.com/Angsa077'} className='hover:text-accent transition-all duration-300'>
             <RiFacebookLine />
         </Link>
-        <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <Link href={'https://www.youtube.com/@angsa_77'} className='hover:text-accent transition-all duration-300'>
             <RiYoutubeLine />
         </Link>
     </div>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,6 @@
-// icons
+import React, { useState } from 'react';
+
+//icons
 import {
   FaHtml5,
   FaCss3,
@@ -6,18 +8,18 @@ import {
   FaReact,
   FaWordpress,
   FaFigma,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 import {
   SiNextdotjs,
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
-} from "react-icons/si";
+} from 'react-icons/si';
 
 
-//  data
-const aboutData = [
+//about data
+export const aboutData = [
   {
     title: 'skills',
     info: [
@@ -88,8 +90,102 @@ const aboutData = [
   },
 ];
 
+//components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+//framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+//counters
+import CountUp from 'react-countup';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+  <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+      <Circles />
+      {/* avatar img */}
+      <motion.div 
+          variants={fadeIn('right', 0.2)} 
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='hidden xl:flex absolute bottom-0 -left-[370px]'
+          >
+          <Avatar />
+      </motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+          {/* text */}
+          <div className='flex-1 flex flex-col justify-center'>
+            <motion.h2 
+                variants={fadeIn('right', 0.2)} 
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='h2'>Web <span className='text-accent'>Developer</span>.
+            </motion.h2>
+            <motion.p 
+                variants={fadeIn('right', 0.4)} 
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+                  1 years ago, I began freelancing as a web developer. 
+                  Since then, i've done work for a few company. 
+            </motion.p>
+          </div>
+          {/* info */}
+          <motion.div 
+                variants={fadeIn('left', 0.4)} 
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='flex flex-col w-full xl:max-w-[48%] h-[180px]'>
+              <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+                  {aboutData.map((item, itemIndex) =>{
+                    return (
+                      <div
+                          key={itemIndex}
+                          className={`${index === itemIndex && 'text-accent after:w-[100%] after:transition-all after:duration-300'}
+                          cursor-pointer capitalize xl:text-lg relative after:w-8
+                          after:h-[2px] after:bg-white after:absolute after:-bottom-1
+                          after:left-0`}
+                          onClick={() => setIndex(itemIndex)}
+                      >
+                        {item.title}
+                      </div>
+                    );
+                  })}
+              </div>
+              <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+                  {aboutData[index].info.map((item, itemIndex) => {
+                    return (
+                      <div 
+                      key={itemIndex}
+                      className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 
+                      items-center text-white/60'
+                      >
+                        {/* title */}
+                          <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                          <div className='hidden md:flex'>-</div>
+                          <div>{item.stage}</div>
+                          <div className='flex gap-x-4'>
+                            {/* icons */}
+                            {item.icons?.map((icon, itemIndex) => {
+                              return <div className='text-2xl text-white'>{icon}</div>
+                            })}
+                          </div>
+                      </div>
+                    );
+                  })}
+              </div>
+          </motion.div>
+      </div>
+  </div>
+  );
 };
 
 export default About;
