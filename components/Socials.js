d450328a60fad4ba4960014662bb5cba398a7875
Angga Saputra <angsa0077@gmail.com>
2023-08-25 11:49:41 +0700

    header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,35 @@
+//links
+import Link from "next/link";
+
+//icons
+import { 
+  RiGithubLine, 
+  RiInstagramLine, 
+  RiTiktokLine, 
+  RiFacebookLine,  
+  RiYoutubeLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className='flex items-center gap-x-5 text-lg'>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiGithubLine />
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiInstagramLine />
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiTiktokLine />
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiFacebookLine />
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiYoutubeLine />
+        </Link>
+    </div>
+  );
 };
 
 export default Socials;
